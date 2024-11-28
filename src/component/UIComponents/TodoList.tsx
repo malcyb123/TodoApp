3d185d6c5d24e77bb05376ee627d30760f4995e7
Malik <mosaddekamallick@gmail.com>
2024-11-28 13:46:18 +0530

    Activity indicator

--- src/component/UIComponents/TodoList.tsx
+++ src/component/UIComponents/TodoList.tsx
@@ -1,18 +1,29 @@
 import { useCallback, useEffect, useRef } from "react";
 import TodoItem from "./TodoListCard";
 import { FlatList } from "react-native-gesture-handler";
-import { Text } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { Todo } from "../../utils/types";
 import { useDispatch, useSelector } from "react-redux";
 import { resetScrollToTop } from "../../redux/TodoSlice";
 
-const TodoList = ({ todos, navigation, toggleCompletion, handleDelete, styles, isLoading, hasMore, setPage }: any) => {
-    const dispatch = useDispatch();
-    const shouldScrollToTop = useSelector((state: any) => state.todos.shouldScrollToTop);
-  const flatListRef = useRef<FlatList>(null);  // Create a reference to the FlatList
+const TodoList = ({
+  todos,
+  navigation,
+  toggleCompletion,
+  handleDelete,
+  styles,
+  isLoading,
+  hasMore,
+  setPage,
+}: any) => {
+  const dispatch = useDispatch();
+  const shouldScrollToTop = useSelector(
+    (state: any) => state.todos.shouldScrollToTop
+  );
+  const flatListRef = useRef<FlatList>(null); // Create a reference to the FlatList
 
-   // Scroll to top when the flag is true
-   const scrollToTop = () => {
+  // Scroll to top when the flag is true
+  const scrollToTop = () => {
     if (flatListRef.current) {
       flatListRef.current.scrollToOffset({ animated: true, offset: 0 });
       dispatch(resetScrollToTop()); // Reset scroll state after triggering
@@ -26,21 +37,24 @@ const TodoList = ({ todos, navigation, toggleCompletion, handleDelete, styles, i
     }
   }, [shouldScrollToTop]);
 
-  const renderItem = useCallback(({ item }: { item: Todo }) => {
-    return (
-      <TodoItem
-        item={item}
-        navigation={navigation}
-        toggleCompletion={toggleCompletion}
-        handleDelete={handleDelete}
-        styles={styles}
-      />
-    );
-  }, [navigation, toggleCompletion, handleDelete, styles]);
+  const renderItem = useCallback(
+    ({ item }: { item: Todo }) => {
+      return (
+        <TodoItem
+          item={item}
+          navigation={navigation}
+          toggleCompletion={toggleCompletion}
+          handleDelete={handleDelete}
+          styles={styles}
+        />
+      );
+    },
+    [navigation, toggleCompletion, handleDelete, styles]
+  );
 
   return (
     <FlatList
-      ref={flatListRef}  // Attach ref
+      ref={flatListRef} // Attach ref
       data={todos}
       keyExtractor={(item) => item.id.toString()}
       renderItem={renderItem}
@@ -50,7 +64,14 @@ const TodoList = ({ todos, navigation, toggleCompletion, handleDelete, styles, i
         }
       }}
       onEndReachedThreshold={0.5}
-      ListFooterComponent={isLoading ? <Text>Loading...</Text> : null}
+      ListFooterComponent={
+        isLoading ? (
+          <View style={{ paddingVertical: 10, alignItems: "center" }}>
+            <ActivityIndicator size="small" color="#000" />
+            <Text>Loading...</Text>
+          </View>
+        ) : null
+      }
       initialNumToRender={10}
       maxToRenderPerBatch={10}
       windowSize={5}
